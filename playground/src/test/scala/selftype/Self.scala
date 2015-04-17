bdb1ccf49b62c486d2baa3ee93e3084b5c5e383c
1ambda <1amb4a@gmail.com>
2015-04-17 21:57:24 +0900

    cake pattern using self type

---
+++ playground/src/test/scala/selftype/Self.scala
@@ -0,0 +1,40 @@
+package selftype
+
+import org.scalatest._
+
+// http://marcus-christie.blogspot.kr/2014/03/scala-understanding-self-type.html
+// https://coderwall.com/p/t_rapw/cake-pattern-in-scala-self-type-annotations-explicitly-typed-self-references-explained
+class Self extends FlatSpec with Matchers {
+
+  trait Connector {
+    def getConnection: String 
+  }
+
+  trait MysqlConnector extends Connector {
+    def getConnection = "Mysql Connection"
+  }
+
+  trait MongoConnector extends Connector {
+    def getConnection = "Mongo Connection"
+  }
+
+  class UserService {
+    this: Connector =>
+
+    def getAllUsers() = {
+      val con = getConnection
+      // do something using the connection
+      "users returned using " + con
+    }
+  }
+
+  "UserServiceMongo" should "return users using mongo connector" in  {
+    val UserServiceMongo = new UserService with MongoConnector
+    UserServiceMongo.getAllUsers should be ("users returned using Mongo Connection")
+  }
+
+  "UserServiceMysql" should "return users using mysql connector" in  {
+    val UserServiceMysql = new UserService with MysqlConnector
+    UserServiceMysql.getAllUsers should be ("users returned using Mysql Connection")
+  }
+}
