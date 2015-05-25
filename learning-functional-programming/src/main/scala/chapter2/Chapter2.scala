c123998594688a0bdfa7d81f8bab3943551c2b09
1ambda <1amb4a@gmail.com>
2015-05-25 13:03:33 +0900

    FP Chapter 1, 2

--- learning-functional-programming/src/main/scala/chapter2/Chapter2.scala
+++ learning-functional-programming/src/main/scala/chapter2/Chapter2.scala
@@ -19,4 +19,45 @@ object Chapter2 {
 
   def formatAbs(n: Int) = format(s"abs($n): ", n, abs)
   def formatFactorial(n: Int) = format(s"factorial(${n}): ", n, factorial)
+
+
+  def findFirst[A](p: A => Boolean, xs: List[A]): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (xs.length < n) -1
+      else if (p(xs(n))) n
+      else loop(n + 1)
+    }
+
+    loop(0)
+  }
+
+  def isSorted[A](arr: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= arr.length) true
+      else if (ordered(arr(n), arr(n+1))) loop(n+1)
+      else false
+    }
+
+    loop(0)
+  }
+
+  def lessThan1(x: Int, y: Int): Boolean = x < y
+  val lessThan2 = new Function2[Int, Int, Boolean] {
+    override def apply(x: Int, y: Int) = x < y
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
+    (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    g andThen f
+  /* (a: A) => f(g(a)) */
 }
--- learning-functional-programming/src/test/scala/chapter2/Chapter2Spec.scala
+++ learning-functional-programming/src/test/scala/chapter2/Chapter2Spec.scala
@@ -13,4 +13,29 @@ class Chapter2Spec extends FunSuite with Matchers {
     formatFactorial(10) should be ("factorial(10): 3628800")
     formatAbs(-10) should be ("abs(-10): 10")
   }
+
+  test("findFirst") {
+    val ns = List(1, 2, 3, 4)
+    val ss = List('a', 'b', 'c', 'd')
+
+    findFirst((x: Int) => x == 2  , ns) should be (1)
+    findFirst((c: Char) => c == 'd', ss) should be (3)
+  }
+
+  test("isSorted") {
+    val ws = Array(1, 2, 4, 3)
+    val xs = Array(4, 3, 2, 1)
+    val ys = Array(0, 0, 1, -1)
+    val zs = Array(-1, 2, 0)
+
+    val fixtures = List(ws, xs, ys, zs)
+    val pred = (x: Int, y: Int) => x < y
+
+    val result = fixtures.map(isSorted(_, pred))
+    result should not contain (true)
+  }
+
+  test("lessThan") {
+    lessThan1(1, 3) should be (lessThan2(1, 3))
+  }
 }
