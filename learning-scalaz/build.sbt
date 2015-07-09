eb14a6225e6576e0a918a2eb3beeb0972c9ac5bd
1ambda <1amb4a@gmail.com>
2015-07-09 09:43:40 +0900

    test Functor.void

--- learning-scalaz/build.sbt
+++ learning-scalaz/build.sbt
@@ -4,7 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
+val scalazVersion = "7.2.0-M2"
+
 libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "7.1.3",
+  "org.scalaz" %% "scalaz-core" % scalazVersion,
+  "org.scalaz" %% "scalaz-concurrent" % scalazVersion,
   "org.scalatest" % "scalatest_2.11" % "3.0.0-M5"
 )
--- learning-scalaz/src/test/scala/functor/FunctorSpec.scala
+++ learning-scalaz/src/test/scala/functor/FunctorSpec.scala
@@ -6,7 +6,10 @@ import scalaz.Functor
 import scalaz.std.option._
 import scalaz.syntax.functor._
 import scalaz.std.list._
+import scalaz.syntax.equal._
+import scalaz.concurrent.Task
 
+// ref: https://github.com/scalaz/scalaz/blob/series/7.2.x/example/src/main/scala/scalaz/example/FunctorUsage.scala
 class FunctorSpec extends FlatSpec with Matchers {
 
   val len: String => Int = _.length
@@ -26,11 +29,54 @@ class FunctorSpec extends FlatSpec with Matchers {
     lenOption(Some("abcd")) shouldBe Some(4)
   }
 
-  "Functor" should "provide strengthL, R functions" in {
+  "Functor" should "provides strengthL, R functions" in {
     Functor[List].strengthL("a", List(1, 2, 3)) shouldBe List("a" -> 1, "a" -> 2, "a" -> 3)
     Functor[List].strengthR(List(1, 2, 3), "a") shouldBe List(1 -> "a", 2 -> "a", 3 -> "a")
 
     List(1, 2, 3).strengthL("a") shouldBe List("a" -> 1, "a" -> 2, "a" -> 3)
     List(1, 2, 3).strengthR("a") shouldBe List(1 -> "a", 2 -> "a", 3 -> "a")
+  }
+
+  "Functor" should "provides a fproduct function" in {
+    val source = List("a", "aa", "b", "ccccc")
+    val result = List("a" -> 1, "aa" -> 2, "b" -> 1, "ccccc" -> 5)
+
+    source.fproduct(len) shouldBe result
+  }
+
+  "Functor" should "provides void" in {
+    Functor[Option].void(Some(1)) shouldBe Some()
+  }
+
+  "Functor.void" can "execute the side-effects" in {
+    // pretend this is our database
+    var database = Map("abc" -> 1, "aaa" -> 2, "qqq" -> 3)
+
+    def deleteRow(f: String => Boolean): Task[Int] = Task.delay {
+      val (count, db) = database.foldRight(0 -> List.empty[(String, Int)]) {
+        case ((key, value), (deleted, result)) if f(key) => (deleted + 1, result)
+        case (row, (deleted, result)) => (deleted, row :: result)
+      }
+
+      database = db.toMap
+      count
+    }
+
+    val deleteTask = deleteRow(_.startsWith("a"))
+
+    // it hasn't run yet
+    database.size shouldBe 3
+
+    // same as, Functor[Task].void(deleteTask)
+    val voidTask: Task[Unit] = deleteTask.void
+
+    voidTask.run shouldBe ()
+
+    database.size shouldBe 1
+  }
+
+  "Functor" can "be albe to be composed" in {
+    val listOpt = Functor[List] compose Functor[Option]
+    listOpt.map(List(Some(1), None, Some(3)))(_ + 1) shouldBe List(Some(2), None, Some(4))
   }
 }
