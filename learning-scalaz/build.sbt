155f64fcfc3f891a03080370893ea988348bcd54
1ambda <1amb4a@gmail.com>
2015-07-15 09:58:14 +0900

    add Scalaz State monad examples

--- learning-scalaz/build.sbt
+++ learning-scalaz/build.sbt
@@ -9,5 +9,8 @@ val scalazVersion = "7.2.0-M2"
 libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % scalazVersion,
   "org.scalaz" %% "scalaz-concurrent" % scalazVersion,
+  "org.scalaz" %% "scalaz-effect" % scalazVersion,
+  "org.scalaz" %% "scalaz-typelevel" % "7.1.3",
+  "org.scalaz" %% "scalaz-scalacheck-binding" % scalazVersion,
   "org.scalatest" % "scalatest_2.11" % "3.0.0-M5"
 )
---
+++ learning-scalaz/src/test/scala/StateMonadSpec.scala
@@ -0,0 +1,108 @@
+import org.scalatest.{Matchers, FunSuite}
+
+import scalaz._, Scalaz._
+
+/*
+
+  ref: https://softwarecorner.wordpress.com/2014/12/04/scalaz-statet-monad-transformer/
+
+  It's important to realize that this function is not a method on State but
+  rather a data field within the State object.
+
+  That is, different State objects can have different functions inside them.
+
+ */
+
+class StateMonadSpec extends FunSuite with Matchers{
+
+  def len = State { s: String => (s, s.size) }
+  def repeat(num: Int): State[String, Unit] = State { s: String => (s * num, ()) }
+
+  test("State test") {
+    len.run("hello") shouldBe ("hello", 5)
+    repeat(3).run("hello") shouldBe ("hello" * 3, ())
+  }
+
+  // def flatMap[B](f: A => State[S, B]): State[S, B]
+  test("flatMap test") {
+    len.flatMap(repeat).run("hello") shouldBe ("hello" * 5, ())
+
+    len.flatMap(repeat).flatMap(_ => len).run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+  // def get[S] = State { s: S => (s, s) }
+  // def map[B](f: A => B): State[S, B]
+  test("get, map test") {
+    get[String]
+      .flatMap(s0 => repeat(s0.size))
+      .flatMap(_ => get[String])
+      .map(s1 => s1.size)
+      .run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+  // def put[S](newState: S): State { s: S => (newState, ()) }
+  test("get, put") {
+    get[String]
+      .flatMap(s0 => put(s0 * s0.size))
+      .flatMap(_ => get[String])
+      .map(s1 => s1.size)
+      .run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+  test("for comprehension") {
+    val m = for {
+      s0 <- get[String]
+      _ <- put(s0 * s0.size)
+      s1 <- get[String]
+    } yield s1.size
+
+    m.run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+
+  /*
+    In for comprehension solution, it fetches the current state and stores it in s0.
+    Then it changes the state with the call to put(), meaning that the state in s0 is now obsolete.
+    But s0 is still in scope. So, we can continue to use th state in s0. even though it's obsolete.
+
+    It would be easy to accidentally yield s0.size instead of s1.size
+
+    So, we need to use modify instead of put.
+
+    def modify[S](f: S => S) = State { s: S => (f(s), ()) }
+   */
+
+  test("modify test") {
+    val m = for {
+      _ <- modify { s: String => s * s.size}
+      s1 <- get[String]
+    } yield s1.size
+
+    m.run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+  /*
+    We can get ride of s1 using functions gets()
+
+    def gets[S, A](f: S => A) = State { s: S => (s, f(s)) }
+   */
+
+  test("gets test") {
+    val m = for {
+      _ <- modify { s: String => s * s.size }
+      size <- gets { s: String => s.size }
+    } yield size
+
+    m.run("hello") shouldBe ("hello" * 5, 25)
+  }
+
+  // ref: https://softwarecorner.wordpress.com/2013/08/29/scalaz-state-monad/
+  test("wordcount example 1") {
+    // todo words => Source.fromFile("build.sbt")
+//    def wordCounts(str: String, currMap: Map[String, Int]): Map[String, Int] = {
+//      words(str).foldLeft(currMap) { (map, word) =>
+//
+//      }
+//    }
+  }
+}
