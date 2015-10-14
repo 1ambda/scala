34c211607b5b465928cf2bb8ecb7b0357e6d557c
1ambda <1amb4a@gmail.com>
2015-10-14 21:34:31 +0900

    traverseU, traverseS usage added

--- learning-scalaz/src/main/scala/monad_transformer/GithubService.scala
+++ learning-scalaz/src/main/scala/monad_transformer/GithubService.scala
@@ -6,6 +6,8 @@ case class User(name: String, repositories: List[Repository])
 case class Repository(name: String, languages: List[Language])
 case class Language(name: String, line: Long)
 
+case class LanguageLookup(userName: String, repoName: String, langName: String)
+
 
 object GithubService {
   def findLanguage1(users: List[User],
@@ -20,26 +22,75 @@ object GithubService {
 
   type LangState[A] = State[List[Language], A]
 
-def findLanguage2(users: List[User],
-                  userName: String,
-                  repoName: String,
-                  langName: String): LangState[Option[Language]] =
-  for {
-    optUser <- (users.find { _.name === userName }).point[LangState]
-    optRepository <- (
-      optUser match {
-        case Some(u) => u.repositories.find(_.name === repoName)
-        case None => none[Repository] // same as Option.empty[Repository]
+  def findLanguage2(users: List[User],
+                    userName: String,
+                    repoName: String,
+                    langName: String): LangState[Option[Language]] =
+    for {
+      optUser <- (users.find { _.name === userName }).point[LangState]
+      optRepository <- (
+        optUser match {
+          case Some(u) => u.repositories.find(_.name === repoName)
+          case None => none[Repository] // same as Option.empty[Repository]
+        }).point[LangState]
+      optLanguage <- (optRepository match {
+        case Some(r) => r.languages.find(_.name === langName)
+        case None    => none[Language]
       }).point[LangState]
-    optLanguage <- (optRepository match {
-      case Some(r) => r.languages.find(_.name === langName)
-      case None    => none[Language]
-    }).point[LangState]
-    _ <- modify { langs: List[Language] => optLanguage match {
-      case Some(l) => l :: langs
-      case None    => langs
-    }}
-  } yield optLanguage
+      _ <- modify { langs: List[Language] => optLanguage match {
+        case Some(l) if (l.line >= 1000) =>  l :: langs
+        case _                           => langs
+      }}
+    } yield optLanguage
 
+  case class LangStateOption[A](run: LangState[Option[A]])
 
+  implicit val LangStateOptionMonad = new Monad[LangStateOption] {
+    override def point[A](a: => A): LangStateOption[A] =
+      LangStateOption(a.point[Option].point[LangState])
+
+    override def bind[A, B](fa: LangStateOption[A])(f: (A) => LangStateOption[B]): LangStateOption[B] =
+      LangStateOption(fa.run.flatMap { (o: Option[A]) => o match {
+        case Some(a) => f(a).run
+        case None    => (none[B]).point[LangState] /* same as `(None: Option[B]).point[LangState]` */
+      }})
+  }
+
+  def findLanguage3(users: List[User],
+                    userName: String,
+                    repoName: String,
+                    langName: String): LangStateOption[Language] =
+    for {
+      u <- LangStateOption((users.find { _.name === userName }).point[LangState])
+      r <- LangStateOption((u.repositories.find { _.name === repoName }).point[LangState])
+      l <- LangStateOption((r.languages.find { _.name === langName }).point[LangState])
+      _ <- LangStateOption((modify { langs: List[Language] =>
+        if (l.line >= 1000) l :: langs else langs
+      }) map (_ => none[Language]))
+    } yield l
+
+  def findLanguage(users: List[User],
+                   userName: String,
+                   repoName: String,
+                   langName: String): OptionT[LangState, Language] =
+    for {
+      u <- OptionT((users.find { _.name === userName }).point[LangState])
+      r <- OptionT((u.repositories.find { _.name === repoName }).point[LangState])
+      l <- OptionT((r.languages.find { _.name === langName }).point[LangState])
+      _ <- modify { langs: List[Language] =>
+        if (l.line >= 1000) l :: langs else langs
+      }.liftM[OptionT]
+    } yield l
+
+  def findLanguages1(users: List[User],
+                     lookups: List[LanguageLookup]): OptionT[LangState, List[Language]] =
+    lookups.traverseU { lookup =>
+      findLanguage(users, lookup.userName, lookup.repoName, lookup.langName)
+    }
+
+  def findLanguages2(users: List[User],
+                     lookups: List[LanguageLookup]): LangState[List[Option[Language]]] =
+    lookups.traverseS { lookup =>
+      findLanguage(users, lookup.userName, lookup.repoName, lookup.langName).run
+    }
 }
--- learning-scalaz/src/test/scala/monad_transformer/OptionTSpec.scala
+++ learning-scalaz/src/test/scala/monad_transformer/OptionTSpec.scala
@@ -18,9 +18,62 @@ class OptionTSpec extends TestUtils with OptionTSpecFixtures {
     l4.isDefined shouldBe false
   }
 
+  "findLanguage2" in {
+    val s = for {
+      o1 <- findLanguage2(users, "1ambda", "akka", "bash")
+      o2 <- findLanguage2(users, "1ambda", "akka", "java")
+    } yield o1
 
+//    println(s.runZero[List[Language]])
+  }
 
-  OptionT
+  "findLanguage3" in {
+    val s = for {
+      o1 <- findLanguage3(users, "1ambda", "akka", "bash")
+      o2 <- findLanguage3(users, "1ambda", "akka", "java")
+    } yield o1
+
+//    println(s.run.runZero[List[Language]])
+  }
+
+  "liftM" in {
+    // type LangState[A] = State[List[Language], A]
+    val l = Language("lisp", 309)
+    val os1: OptionT[LangState, Language] = l.point[LangState].liftM[OptionT]
+    val os2: OptionT[LangState, Language] = OptionT(l.some.point[LangState])
+
+    os1 === os2
+    os1.run === os2.run
+    os1.run.runZero[List[Language]] === os2.run.runZero[List[Language]]
+  }
+
+  "findLanguage with OptionT" in {
+    val s1 = for {
+      o1 <- findLanguage3(users, "1ambda", "akka", "bash")
+      o2 <- findLanguage3(users, "1ambda", "akka", "java")
+    } yield o1
+
+    val s2 = for {
+      o1 <- findLanguage(users, "1ambda", "akka", "bash")
+      o2 <- findLanguage(users, "1ambda", "akka", "java")
+    } yield o1
+
+    s1.run === s2.run
+  }
+
+  "findLanguages" in {
+    val lookups = List(
+      LanguageLookup("1ambda", "akka", "scala"),
+      LanguageLookup("1ambda", "akka", "erlang?!")
+    )
+
+    val (longLangs1, optFoundLangs1) = findLanguages1(users, lookups).run.runZero
+    optFoundLangs1 shouldBe none
+
+    val (longLangs2, foundOptLangs2) = findLanguages2(users, lookups).runZero
+    foundOptLangs2.length shouldBe 2
+    foundOptLangs2 shouldBe List(Language("scala", 4990).some, none)
+  }
 }
 
 trait OptionTSpecFixtures {
