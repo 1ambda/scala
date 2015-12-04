065fecacab59eede0e3f1b3e8056926ea1a1c0d0
1ambda <1amb4a@gmail.com>
2015-12-04 17:47:53 +0900

    Implement bind using 'join' in Monad.scala

--- learning-scalaz/src/main/scala/free/withoutScalaz/Monad.scala
+++ learning-scalaz/src/main/scala/free/withoutScalaz/Monad.scala
@@ -1,13 +1,22 @@
 package free.withoutScalaz
 
-trait Monad[M[_]] {
-  def unit[A](a: A): M[A]
-  def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
-  def map[A, B](ma: M[A])(f: A => B): M[B]
+trait Monad1[F[_]] {
+  def point[A](a: A): F[A]
+  def bind[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def join[A](ffa: F[F[A]]): F[A] =
+    bind(ffa)(a => a)
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    bind(fa)(a => point(f(a)))
 }
 
-object Monad {
-  def apply[M[_]: Monad]: Monad[M] =  implicitly[Monad[M]]
+trait Monad2[F[_]] {
+  def point[A](a: A): F[A]
+  def join[A](ffa: F[F[A]]): F[A]
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def bind[A, B](fa: F[A])(f: A => F[B]): F[B] =
+    join(map(fa)(f))
 }
 
 
