a1a2fe7bfc02acfffd3c442c92097a508c44f38e
1ambda <1amb4a@gmail.com>
2015-12-06 15:43:59 +0900

    Update comment on Coyoneda

--- learning-scalaz/src/main/scala/yoneda/Yoneda.scala
+++ learning-scalaz/src/main/scala/yoneda/Yoneda.scala
@@ -5,61 +5,68 @@ import scalaz.Functor
 object Yoneda {
   // https://github.com/scalaz/scalaz/blob/series/7.1.x/core/src/main/scala/scalaz/Yoneda.scala
 
-  abstract class Yoneda[F[_], A] { yo =>
-    def apply[B](f: A => B): F[B]
+abstract class Yoneda[F[_], A] { yo =>
+  def apply[B](f: A => B): F[B]
 
-    def run: F[A] = apply(a => a)
+  def run: F[A] = apply(a => a)
 
-    def map[B](f: A => B): Yoneda[F, B] = new Yoneda[F, B] {
-      override def apply[C](g: (B) => C): F[C] = yo(f andThen g)
-    }
+  def map[B](f: A => B): Yoneda[F, B] = new Yoneda[F, B] {
+    override def apply[C](g: (B) => C): F[C] = yo(f andThen g)
+  }
+}
+
+/** `Yoneda[F, _]` is a functor for any `F` */
+implicit def yonedaFunctor[F[_]]: Functor[({ type  λ[α] = Yoneda[F,α]})#λ] =
+  new Functor[({type λ[α] = Yoneda[F, α]})#λ] {
+    override def map[A, B](ya: Yoneda[F, A])(f: A => B): Yoneda[F, B] =
+      ya map f
   }
 
-  /** `Yoneda[F, _]` is a functor for any `F` */
-  implicit def yonedaFunctor[F[_]]: Functor[({ type  λ[α] = Yoneda[F,α]})#λ] =
-    new Functor[({type λ[α] = Yoneda[F, α]})#λ] {
-      override def map[A, B](ya: Yoneda[F, A])(f: A => B): Yoneda[F, B] =
-        ya map f
-    }
+/** `F[A]` converts to `Yoneda[F, A]` for any functor `F` */
+def apply[F[_]: Functor, A](fa: F[A]): Yoneda[F, A] = new Yoneda[F, A] {
+  override def apply[B](f: A => B): F[B] = Functor[F].map(fa)(f)
+}
 
-  /** `F[A]` converts to `Yoneda[F, A]` for any functor `F` */
-  def apply[F[_]: Functor, A](fa: F[A]): Yoneda[F, A] = new Yoneda[F, A] {
-    override def apply[B](f: A => B): F[B] = Functor[F].map(fa)(f)
-  }
-
-  /** `Yoneda[F, A]` converts to `F[A` for any `F` */
-  def from[F[_], A](yo: Yoneda[F, A]): F[A] =
-    yo.run
+/** `Yoneda[F, A]` converts to `F[A` for any `F` */
+def from[F[_], A](yo: Yoneda[F, A]): F[A] =
+  yo.run
 }
 
 object Coyoneda {
-  sealed abstract class Coyoneda[F[_], A] { coyo =>
-    type I
-    val fi: F[I]
-    val k: I => A
+sealed abstract class Coyoneda[F[_], A] { coyo =>
+  type I
+  val fi: F[I]
+  val k: I => A
 
-    final def map[B](f: A => B): Aux[F, I, B] =
-      apply(fi)(f compose k)
+  final def map[B](f: A => B): Aux[F, I, B] =
+    apply(fi)(f compose k)
 
-    final def run(implicit F: Functor[F]): F[A] =
-      F.map(fi)(k)
+  final def run(implicit F: Functor[F]): F[A] =
+    F.map(fi)(k)
+}
+
+type Aux[F[_], A, B] = Coyoneda[F, B] { type I = A }
+
+def apply[F[_], A, B](fa: F[A])(_k: A => B): Aux[F, A, B] =
+  new Coyoneda[F, B] {
+    type I = A
+    val k = _k
+    val fi = fa
   }
 
-  type Aux[F[_], A, B] = Coyoneda[F, B] { type I = A }
+/** `F[A]` converts to `Coyoneda[F, A]` for any `F` */
+def lift[F[_], A](fa: F[A]): Coyoneda[F, A] = apply(fa)(identity[A])
 
-  def apply[F[_], A, B](fa: F[A])(_k: A => B): Aux[F, A, B] =
-    new Coyoneda[F, B] {
-      type I = A
-      val k = _k
-      val fi = fa
-    }
+/** `Coyoneda[F, A]` converts to `F[A]` for any Functor `F` */
+def from[F[_], A](coyo: Coyoneda[F, A])(implicit F: Functor[F]): F[A] =
+  F.map(coyo.fi)(coyo.k)
 
-  /** `F[A]` converts to `Coyoneda[F, A]` for any `F` */
-  def lift[F[_], A](fa: F[A]): Coyoneda[F, A] = apply(fa)(identity[A])
-
-  /** `Coyoneda[F, A]` converts to `F[A]` for any Functor `F` */
-  def from[F[_], A](coyo: Coyoneda[F, A])(implicit F: Functor[F]): F[A] =
-    F.map(coyo.fi)(coyo.k)
+/** `CoyoYoneda[F, _]` is a functor for any `F` */
+implicit def coyonedaFunctor[F[_]]: Functor[({ type  λ[α] = Coyoneda[F,α]})#λ] =
+  new Functor[({type λ[α] = Coyoneda[F, α]})#λ] {
+    override def map[A, B](ca: Coyoneda[F, A])(f: A => B): Coyoneda[F, B] =
+      ca.map(f)
+  }
 }
 
 object Yoneda1 {
@@ -127,5 +134,4 @@ object Yoneda1 {
     def fromCoyoneda[F[_] : Functor, A](coyo: Coyoneda[F, A]): F[A] =
       Functor[F].map(coyo.fi)(coyo.f)
   }
-
 }
